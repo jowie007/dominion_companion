77eb318ea15d0dad341e065ccca11fe6b04830dc
Jonas Wiegmann <jonas.wiegmann@adesso.de>
2023-06-18 22:22:44 +0200

    added audio and new application name

--- lib/services/music_service.dart
+++ lib/services/music_service.dart
@@ -1,7 +1,16 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/cupertino.dart';
 
+// TODO Enable looping
+// TODO Enable notification
+// https://pub.dev/packages/assets_audio_player
 class MusicService {
   static final MusicService _musicService = MusicService._internal();
+
+  final assetsAudioPlayer = AssetsAudioPlayer();
+  final Audio audio = Audio("assets/audio/dominion.wav");
+
+  bool initialized = false;
 
   factory MusicService() {
     return _musicService;
@@ -13,7 +22,16 @@ class MusicService {
 
   ValueNotifier<bool> notifier = ValueNotifier(false);
 
+  void init() {
+    assetsAudioPlayer.open(audio);
+    initialized = true;
+  }
+
   void togglePlaying() {
+    if (!initialized) {
+      init();
+    }
+    assetsAudioPlayer.playOrPause();
     isPlaying = !isPlaying;
     notifier.value = !notifier.value;
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import assets_audio_player
+import assets_audio_player_web
 import device_info_plus_macos
 import file_selector_macos
 import package_info_plus
@@ -13,6 +15,8 @@ import pdfx
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AssetsAudioPlayerPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerPlugin"))
+  AssetsAudioPlayerWebPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerWebPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
