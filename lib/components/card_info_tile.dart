9595c64e2a0312b5b82d35a55765a92efbfc6b8c
Jonas Wiegmann <jonas.wiegmann@adesso.de>
2024-06-06 10:11:32 +0200

    added basisspiel v1 cards

--- lib/components/card_info_tile.dart
+++ lib/components/card_info_tile.dart
@@ -39,173 +39,184 @@ class _CardInfoTileState extends State<CardInfoTile> {
     final cardColors = cardService.getColorsByCardTypeString(cardTypeString);
     final PlayerService playerService = PlayerService();
 
-    return Container(
-      color: Colors.white.withOpacity(0.4),
-      child: Theme(
-        data: theme.copyWith(checkboxTheme: newCheckBoxTheme),
-        child: ListTile(
-          onTap: () => widget.onChanged(false),
-          onLongPress: () => {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return FutureBuilder(
-                  future: cardService.getCardIdsForPopup(widget.card),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasData && snapshot.data != null) {
-                      return CardPopup(
-                          cardIds: snapshot.data!,
-                          expansionId: snapshot.data![0].split("-")[0]);
-                    } else {
-                      return const Text('Karte konnte nicht geladen werden');
-                    }
-                  },
-                );
-              },
-            ),
-          },
-          title: Stack(
-            children: [
-              Row(
-                children: <Widget>[
-                  SizedBox(
-                    width: 74,
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            // https://stackoverflow.com/questions/57699497/how-to-create-a-background-with-stripes-in-flutter
-                            cardColors != null
-                                ? Container(
-                                    width: 68,
-                                    height: 48,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(100),
-                                      gradient: LinearGradient(
-                                        begin: Alignment.topCenter,
-                                        end: Alignment.bottomCenter,
-                                        stops: cardService.getStopsByColors(
-                                            cardColors, 1),
-                                        colors: cardColors,
-                                        tileMode: TileMode.repeated,
-                                      ),
-                                    ),
-                                  )
-                                : Container(),
-                          ],
-                        ),
-                        widget.card.cardCost.debt != ''
-                            ? widget.card.cardCost.coin != ''
-                                ? Positioned(
-                                    top: 3,
-                                    left: 24,
-                                    child: CostComponent(
-                                        width: 40,
-                                        type: 'debt',
-                                        value: widget.card.cardCost.debt),
-                                  )
-                                : Positioned(
-                                    left: 2,
-                                    top: 3,
-                                    child: CostComponent(
-                                        width: 40,
-                                        type: 'debt',
-                                        value: widget.card.cardCost.debt),
-                                  )
-                            : Container(),
-                        widget.card.cardCost.potion != ''
-                            ? widget.card.cardCost.coin != ''
-                                ? const Positioned(
-                                    left: 28,
-                                    top: 3,
-                                    child: CostComponent(
-                                        width: 26, type: 'potion'),
-                                  )
-                                : const Positioned(
-                                    left: 8,
-                                    top: 3,
-                                    child: CostComponent(
-                                        width: 26, type: 'potion'),
-                                  )
-                            : Container(),
-                        widget.card.cardCost.coin != ''
-                            ? Positioned(
-                                top: 3,
-                                left: 2,
-                                child: CostComponent(
-                                    width: 40,
-                                    type: 'coin',
-                                    value: widget.card.cardCost.coin),
-                              )
-                            : const SizedBox(width: 26),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    flex: 8,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.card.name,
-                          style: const TextStyle(
-                              fontSize: 16, color: Colors.black),
-                        ),
-                        Text(
-                          cardTypeString,
-                          style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.black,
-                              fontStyle: FontStyle.italic),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Spacer(),
-                  widget.hasCheckbox
-                      ? RoundCheckbox(
-                          onChanged: widget.onChanged,
-                          value: widget.value,
-                        )
-                      : ExpansionIcon(icon: widget.card.id.split('-')[0])
-                ],
+    // TODO Replace card
+    return Dismissible(
+      key: Key(widget.card.id),
+      // Use card.id as the key
+      direction: DismissDirection.endToStart,
+      // Allow swiping from right to left
+      onDismissed: (direction) {
+        // Implement your logic to replace the card here
+      },
+      child: Container(
+        color: Colors.white.withOpacity(0.4),
+        child: Theme(
+          data: theme.copyWith(checkboxTheme: newCheckBoxTheme),
+          child: ListTile(
+            onTap: () => widget.onChanged(false),
+            onLongPress: () => {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return FutureBuilder(
+                    future: cardService.getCardIdsForPopup(widget.card),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasData && snapshot.data != null) {
+                        return CardPopup(
+                            cardIds: snapshot.data!,
+                            expansionId: snapshot.data![0].split("-")[0]);
+                      } else {
+                        return const Text('Karte konnte nicht geladen werden');
+                      }
+                    },
+                  );
+                },
               ),
-              widget.card.count.isNotEmpty && widget.showCardCount
-                  ? Row(
-                      children: [
-                        const SizedBox(width: 36),
-                        Container(
-                          width: 40,
-                          height: 18,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            color: Colors.white,
+            },
+            title: Stack(
+              children: [
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 74,
+                      child: Stack(
+                        clipBehavior: Clip.none,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // https://stackoverflow.com/questions/57699497/how-to-create-a-background-with-stripes-in-flutter
+                              cardColors != null
+                                  ? Container(
+                                      width: 68,
+                                      height: 48,
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(100),
+                                        gradient: LinearGradient(
+                                          begin: Alignment.topCenter,
+                                          end: Alignment.bottomCenter,
+                                          stops: cardService.getStopsByColors(
+                                              cardColors, 1),
+                                          colors: cardColors,
+                                          tileMode: TileMode.repeated,
+                                        ),
+                                      ),
+                                    )
+                                  : Container(),
+                            ],
                           ),
-                          child: ValueListenableBuilder(
-                            valueListenable: playerService.notifier,
-                            builder: (BuildContext context, bool val,
-                                Widget? child) {
-                              return Text(
-                                widget.card.count[playerService.players - 1],
-                                textAlign: TextAlign.center,
-                              );
-                            },
+                          widget.card.cardCost.debt != ''
+                              ? widget.card.cardCost.coin != ''
+                                  ? Positioned(
+                                      top: 3,
+                                      left: 24,
+                                      child: CostComponent(
+                                          width: 40,
+                                          type: 'debt',
+                                          value: widget.card.cardCost.debt),
+                                    )
+                                  : Positioned(
+                                      left: 2,
+                                      top: 3,
+                                      child: CostComponent(
+                                          width: 40,
+                                          type: 'debt',
+                                          value: widget.card.cardCost.debt),
+                                    )
+                              : Container(),
+                          widget.card.cardCost.potion != ''
+                              ? widget.card.cardCost.coin != ''
+                                  ? const Positioned(
+                                      left: 28,
+                                      top: 3,
+                                      child: CostComponent(
+                                          width: 26, type: 'potion'),
+                                    )
+                                  : const Positioned(
+                                      left: 8,
+                                      top: 3,
+                                      child: CostComponent(
+                                          width: 26, type: 'potion'),
+                                    )
+                              : Container(),
+                          widget.card.cardCost.coin != ''
+                              ? Positioned(
+                                  top: 3,
+                                  left: 2,
+                                  child: CostComponent(
+                                      width: 40,
+                                      type: 'coin',
+                                      value: widget.card.cardCost.coin),
+                                )
+                              : const SizedBox(width: 26),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      flex: 8,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.card.name,
+                            style: const TextStyle(
+                                fontSize: 16, color: Colors.black),
                           ),
-                        ),
-                      ],
-                    )
-                  : Container()
-            ],
+                          Text(
+                            cardTypeString,
+                            style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.black,
+                                fontStyle: FontStyle.italic),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    widget.hasCheckbox
+                        ? RoundCheckbox(
+                            onChanged: widget.onChanged,
+                            value: widget.value,
+                          )
+                        : ExpansionIcon(icon: widget.card.id.split('-')[0])
+                  ],
+                ),
+                widget.card.count.isNotEmpty && widget.showCardCount
+                    ? Row(
+                        children: [
+                          const SizedBox(width: 36),
+                          Container(
+                            width: 40,
+                            height: 18,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Colors.white,
+                            ),
+                            child: ValueListenableBuilder(
+                              valueListenable: playerService.notifier,
+                              builder: (BuildContext context, bool val,
+                                  Widget? child) {
+                                return Text(
+                                  widget.card.count[playerService.players - 1],
+                                  textAlign: TextAlign.center,
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      )
+                    : Container()
+              ],
+            ),
           ),
+          // subtitle: const Text('This is tile number 2'),
         ),
-        // subtitle: const Text('This is tile number 2'),
       ),
     );
   }
